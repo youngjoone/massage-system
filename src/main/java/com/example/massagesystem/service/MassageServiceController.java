2922ed9f2aebc76ff188fb00d919281bcb5617b1
youngjoone <youngjoon_z@naver.com>
2025-07-09 19:25:20 +0900

    cors 403 해결 및 db 인메모리 > 파일

--- src/main/java/com/example/massagesystem/service/MassageServiceController.java
+++ src/main/java/com/example/massagesystem/service/MassageServiceController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/services")
+@RequestMapping("/api/massageservices")
 public class MassageServiceController {
 
     @Autowired
